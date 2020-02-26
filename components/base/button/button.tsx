35505be3b592b9475a8289586bfbc2811005b0c1
OkaKyo <0622okakyo@gmail.com>
2020-02-26 16:14:24 +0900

    コンポーネント追加

--- components/base/button/button.tsx
+++ components/base/button/button.tsx
@@ -1,1 +1,14 @@
 import React from "react";
+
+export class BaseButton extends React.Component{
+    render(){
+        return (
+            <div className="">
+                <button
+            
+                >{}</button>
+            </div>
+        )
+    
+    }
+} 
---
+++ components/complex/header/header.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export class toolbar extends React.Component{
+    render(){
+        return(
+            <div>
+                <h1> Hello World</h1>
+            </div>
+
+        )
+    }
+}
