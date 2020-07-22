8781219a9aa996c89d5d5590593a3b19b91690dc
OkaKyo <0622okakyo@gmail.com>
2020-07-22 11:26:57 +0900

    環境構築中

---
+++ src/components/atoms/buttons/button.tsx
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+
+export const BaseButton: React.FC = () => {
+  const {} = useState();
+
+  return (
+    <div className="button">
+      <button></button>
+    </div>
+  );
+};
---
+++ src/components/atoms/buttons/style.tsx
@@ -0,0 +1,6 @@
+import styled from "@emotion/styled";
+import tw from "tailwind.macro";
+
+const Button = styled.button`
+  ${tw`bg-blue-500 px-5`}
+`;
