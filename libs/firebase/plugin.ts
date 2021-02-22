ca01e19930210470baca34bbd6fbd5f79cd7a2bf
Kyohei Oka <0622okakyo@gmail.com>
2021-02-22 19:10:58 +0900

    docker に対応

--- libs/firebase/plugin.ts
+++ libs/firebase/plugin.ts
@@ -2,16 +2,20 @@ import { processResponsive } from "@chakra-ui/react";
 import Firebase from "firebase/app";
 import "firebase/auth";
 
+
+console.log(process.env)
+
 const FirebaseConfig = {
-  apiKey: process.env.apiKey,
-  authDomain: process.env.authDomain,
-  databaseURL: process.env.databaseURL,
-  projectId: process.env.projectId,
-  storageBucket: process.env.storageBucket,
-  messagingSenderId: process.env.messagingSenderId,
-  appId: process.env.appId,
-  measurementId: process.env.measurementId
+    apiKey: "AIzaSyAXhRANrE1YLJ0FcIxRiFx5gD8npTs27Qc",
+    authDomain: "hudlook-1c8f5.firebaseapp.com",
+    databaseURL: "https://hudlook-1c8f5.firebaseio.com",
+    projectId: "hudlook-1c8f5",
+    storageBucket: "hudlook-1c8f5.appspot.com",
+    messagingSenderId: "965747634693",
+    appId: "1:965747634693:web:eff659395b29318905a26c",
+    measurementId: "G-YP8WN5PL13"
 }
+
 
 const firebase= Firebase.apps.length?Firebase.app():Firebase.initializeApp(FirebaseConfig)
 
--- store/auth.tsx
+++ store/auth.tsx
@@ -1,17 +1,35 @@
 import { Auth } from "@/libs/firebase/plugin";
 import { createContext, useEffect, useState,useContext} from "react"
 import {UserType} from "./user"
-
+import {SignInWithGoogle,SignOut} from "@/libs/firebase"
 
 export type AuthStateType = {
     account?:UserType |null,
+    signOut?:Function,
+    signIn?:Function,
     error?:string
 }
 
+export type AuthAction ={
+    type: "Signin" |"Signout"
+}
 const globalState:AuthStateType = {
     account: null,
     error:""
   }
+
+export const  authReducer = (state:AuthStateType, action: AuthAction)=>{
+    switch(action.type){
+        case "Signin":
+            const user = SignInWithGoogle()
+            return {
+
+            }
+        case "Signout":
+        default:
+            return state
+    }
+}
 
 
 export const AuthContext = createContext<AuthStateType>(globalState);
