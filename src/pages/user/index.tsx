25006ebe52d554c3288824e92154545e31248c14
Kyohei Oka <0622okakyo@gmail.com>
2021-03-16 22:58:45 +0900

    Husky 設定

--- src/pages/user/index.tsx
+++ src/pages/user/index.tsx
@@ -1,11 +1,5 @@
 import { Box, Image,Flex,Heading,Text,Button,Avatar, Grid, GridItem, Spacer, Stack} from '@chakra-ui/react'
 import Head from 'next/head'
-import {gql,useQuery} from "@apollo/client";
-import { UserWindow } from '@/components/templates/user';
-import { useContext } from 'react';
-import { AuthContext } from '@/libs';
-import { GetServerSideProps } from 'next';
-import { useRouter } from 'next/router';
 export default function UserTopPage() {
 
   return (
